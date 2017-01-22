4ca09ed1e403eeddb2d2cfcc337f9fa4cf999f8c
Laurent Tettoni <tettoni@iso.org>
2017-01-22 22:20:09 +0100

    Solve several goals

--- src/main/java/org/logic2j/predsolver/solver/SolverApi.java
+++ src/main/java/org/logic2j/predsolver/solver/SolverApi.java
@@ -1,14 +1,23 @@
 package org.logic2j.predsolver.solver;
 
+import org.logic2j.predsolver.model.Term;
 import org.logic2j.predsolver.model.TermApi;
 import org.logic2j.predsolver.solver.holder.GoalHolder;
+
+import static org.logic2j.predsolver.predicates.Predicates.and;
 
 /**
  * Higher level API.
  */
 public class SolverApi extends Solver {
-  public GoalHolder solve(Object goal) {
-    final Object normalized = TermApi.normalize(goal);
-    return new GoalHolder(this, goal);
+  public GoalHolder solve(Term... goals) {
+    final Object effectiveGoal;
+    if (goals.length==1) {
+      effectiveGoal = goals[0];
+    } else {
+      effectiveGoal = and(goals);
+    }
+    final Object normalized = TermApi.normalize(effectiveGoal);
+    return new GoalHolder(this, normalized);
   }
 }
--- src/test/java/org/logic2j/predsolver/solver/SolverHighLevelTest.java
+++ src/test/java/org/logic2j/predsolver/solver/SolverHighLevelTest.java
@@ -1,6 +1,7 @@
 package org.logic2j.predsolver.solver;
 
 import org.junit.Test;
+import org.logic2j.predsolver.model.Term;
 import org.logic2j.predsolver.model.Var;
 import org.logic2j.predsolver.predicates.Even;
 import org.slf4j.Logger;
@@ -22,28 +23,28 @@ public class SolverHighLevelTest {
   @Test
   public void exists0() {
     final Var<Integer> Q = new Var<>("Q");
-    final Object goal = and(eq(Q, 11), eq(Q, 12));
+    final Term goal = and(eq(Q, 11), eq(Q, 12));
     assertThat(solver.solve(goal).exists(), is(false));
   }
 
   @Test
   public void exists2() {
     final Var<Integer> Q = new Var<>("Q");
-    final Object goal = or(eq(Q, 11), eq(Q, 12));
+    final Term goal = or(eq(Q, 11), eq(Q, 12));
     assertThat(solver.solve(goal).exists(), is(true));
   }
 
   @Test
   public void count0() {
     final Var<Integer> Q = new Var<>("Q");
-    final Object goal = and(eq(Q, 11), eq(Q, 12));
+    final Term goal = and(eq(Q, 11), eq(Q, 12));
     assertThat(solver.solve(goal).count(), is(0L));
   }
 
   @Test
   public void count2() {
     final Var<Integer> Q = new Var<>("Q");
-    final Object goal = or(eq(Q, 11), eq(Q, 12));
+    final Term goal = or(eq(Q, 11), eq(Q, 12));
     assertThat(solver.solve(goal).count(), is(2L));
   }
 
@@ -51,7 +52,7 @@ public class SolverHighLevelTest {
   @Test
   public void wholeSolution() {
     final Var<Integer> Q = new Var<>("Q");
-    final Object goal = new Even(Q);
+    final Term goal = new Even(Q);
     final List<Object> list = solver.solve(goal).solution().list();
     assertThat(list.toString(), is("[even(0), even(2), even(4), even(6), even(8)]"));
   }
@@ -59,7 +60,7 @@ public class SolverHighLevelTest {
   @Test
   public void var() {
     final Var<Integer> Q = new Var<>("Q");
-    final Object goal = new Even(Q);
+    final Term goal = new Even(Q);
     final List<Object> list = solver.solve(goal).var("Q").list();
     assertThat(list.toString(), is("[0, 2, 4, 6, 8]"));
   }
@@ -67,7 +68,7 @@ public class SolverHighLevelTest {
   @Test
   public void vars() {
     final Var<Integer> Q = new Var<>("Q");
-    final Object goal = new Even(Q);
+    final Term goal = new Even(Q);
     final List<Map<Var<?>, Object>> list = solver.solve(goal).vars().list();
     assertThat(list.toString(), is("[{Q=0}, {Q=2}, {Q=4}, {Q=6}, {Q=8}]"));
   }
