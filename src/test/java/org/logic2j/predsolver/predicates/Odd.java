caf70dd5c8e272699d5a1d0ba5f0bea215010cdc
Laurent Tettoni <tettoni@iso.org>
2017-01-24 23:57:01 +0100

    Fix

--- src/test/java/org/logic2j/predsolver/predicates/Odd.java
+++ src/test/java/org/logic2j/predsolver/predicates/Odd.java
@@ -15,7 +15,7 @@ public class Odd extends ListOfValuesFOPredicate1<Integer> {
 
 
   public Odd(Var<Integer> term) {
-    super("odd", term, 0, 2, 4, 6, 8);
+    super("odd", term, 1, 3, 5, 7, 9);
   }
 
 }
