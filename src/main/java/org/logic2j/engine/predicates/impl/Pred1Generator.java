99cda799f033b9770abcdc444ecf5363bcf611f5
Laurent Tettoni <tettoni@iso.org>
2018-05-03 21:34:36 +0200

    Use new UnifyContext.unifyAndNotify()

--- src/main/java/org/logic2j/engine/predicates/impl/Pred1Generator.java
+++ src/main/java/org/logic2j/engine/predicates/impl/Pred1Generator.java
@@ -18,23 +18,16 @@
 package org.logic2j.engine.predicates.impl;
 
 
-import org.logic2j.engine.exception.SolverException;
 import org.logic2j.engine.model.Binding;
 import org.logic2j.engine.model.Constant;
 import org.logic2j.engine.unify.UnifyContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import static java.lang.String.format;
 import static org.logic2j.engine.model.SimpleBindings.bind;
-import static org.logic2j.engine.solver.Continuation.CONTINUE;
 
 /**
  * Generate list of values in sequence.
  */
 public abstract class Pred1Generator<T> extends FOPredicate {
-  private static final Logger logger = LoggerFactory.getLogger(Pred1Generator.class);
-
   private final Constant<T> allowedValues;
 
   public Pred1Generator(String functorName, Binding<T> term, Constant<T> allowedValues) {
@@ -49,26 +42,6 @@ public abstract class Pred1Generator<T> extends FOPredicate {
 
   @Override
   public int predicateLogic(UnifyContext currentVars) {
-    final Object reified = currentVars.reify(getArg(0));
-    if (isFreeVar(reified)) {
-      // Still a free var, we will attempt to read values from the getter and provide bindings
-
-      if (allowedValues != null) {
-        return unifyAndNotifyMany(currentVars, reified, allowedValues.toArray());
-      }
-      return CONTINUE;
-    } else if (isConstant(reified)) {
-      // Variable is bound to a value
-      for (T val : FOPredicate.<T>list(reified)) {
-        final boolean contains = allowedValues.contains(val);
-        final int continuation = notifySolutionIf(contains, currentVars);
-        if (continuation != CONTINUE) {
-          return continuation;
-        }
-      }
-      return CONTINUE;
-    } else {
-      throw new SolverException(format("Should not be handling %s in %s", reified, this));
-    }
+    return currentVars.unifyAndNotify(getArg(0), this.allowedValues);
   }
 }
