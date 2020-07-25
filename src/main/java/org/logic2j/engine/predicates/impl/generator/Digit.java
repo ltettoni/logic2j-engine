8de468a8c495a4a16f1a1c16d6555006ab528633
tettoni <tettoni@iso.org>
2020-07-26 01:05:57 +0200

    Even simpler instantiation of Digit()

--- src/main/java/org/logic2j/engine/predicates/impl/generator/Digit.java
+++ src/main/java/org/logic2j/engine/predicates/impl/generator/Digit.java
@@ -31,6 +31,6 @@ import static org.logic2j.engine.model.SimpleBindings.bind;
 public class Digit extends Pred1Generator<Integer> {
 
   public Digit(Binding<Integer> term) {
-    super("digit", term, bind(0,1,2,3,4,5,6,7,8,9));
+    super("digit", term, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
   }
 }
